df0dd893cd54392fb24c9f73178529d7016d0be0
Priyanshu078 <priyanshupaliwal078@gmail.com>
2021-11-02 22:41:11 +0530

    first commit

---
+++ lib/database.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:task_planner/tasks.dart';
+
+final DatabaseReference = FirebaseDatabase.instance.reference();
+
+saveTasks(Task task) {}
