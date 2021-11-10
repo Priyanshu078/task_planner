460b40be40ce20ef8994c70e6e03230ef0341406
Priyanshu078 <priyanshupaliwal078@gmail.com>
2021-11-10 20:51:18 +0530

    getting saved tasks from the firebase database

--- lib/database.dart
+++ lib/database.dart
@@ -2,8 +2,17 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:task_planner/tasks.dart';
 
 final databaseReference = FirebaseDatabase.instance.reference();
-
+Map tasks = {};
 void saveTasks(String task, String description) {
 //   databaseReference.child("task").set(task);
   databaseReference.push().set({"task1": task, "description": description});
+}
+
+Future<List> getTasks() async {
+  DataSnapshot dataSnapshot = await databaseReference.once();
+  Map data = dataSnapshot.value;
+  List keysList = data.keys.toList();
+  for (var item in keysList) {
+    print(data[item]);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:task_planner/database.dart';
 // import 'package:task_planner/tasks.dart';
 
@@ -28,29 +29,66 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key key, @required this.title}) : super(key: key);
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
+  TextEditingController taskController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: Center(
+        child: FutureBuilder(
+          future: getTasks(),
+        )
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Task task = Task("Study DSAlgo", "Geeks for Geeks");
-          saveTasks("Assigmnet", "Complete Today");
+          taskController.text = "";
+          descriptionController.text = "";
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: const Text("Add Tasks"),
+                    content: Column(
+                      children: <Widget>[
+                        TextField(
+                          controller: taskController,
+                          decoration: const InputDecoration(
+                            hintText: "Enter task",
+                            labelText: "task",
+                          ),
+                        ),
+                        TextField(
+                          controller: descriptionController,
+                          decoration: const InputDecoration(
+                            hintText: "Enter Description",
+                            labelText: "description",
+                          ),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Back")),
+                      TextButton(
+                          onPressed: () {
+                            saveTasks(taskController.text,
+                                descriptionController.text);
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Save")),
+                    ],
+                  ));
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
